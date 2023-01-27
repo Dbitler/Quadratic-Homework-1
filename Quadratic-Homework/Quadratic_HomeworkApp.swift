bd56f3887f04c5282921b382d1d656b4f852277a
Dbitler <dbitler@hawk.iit.edu>
2023-01-27 15:36:47 -0800

    Initial Commit

---
+++ Quadratic-Homework/Quadratic_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quadratic_HomeworkApp.swift
+//  Quadratic-Homework
+//
+//  Created by IIT PHYS 440 on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Quadratic_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
